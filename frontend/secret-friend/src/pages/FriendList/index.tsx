688d6abf69304e67df3566e461810248590ab7b1
bllackdev <rapha_rmm@hotmail.com>
2020-10-08 03:29:01 -0300

    add Header no FriendList

--- frontend/secret-friend/src/pages/FriendList/index.tsx
+++ frontend/secret-friend/src/pages/FriendList/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Aplicando SPA
+
+import logoImg from '../../assets/img/logo.png';
+import backIcon from '../../assets/img/icons/back.svg';
 
 import './styles.css';
 
 function FriendList() {
   return (
-    <h1>FriendList</h1>
+    <div id="page-friend-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Voltar" />
+          </Link>
+          <img src={logoImg} alt="Secret Friend" />
+        </div>
+        <div className="header-content">
+          <strong>Monte sua lista de Amigos.</strong>
+        </div>
+
+      </header>
+    </div>
   )
 }
 
